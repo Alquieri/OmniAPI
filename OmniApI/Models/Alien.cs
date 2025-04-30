10ddb341ba32d14da6599a4c42fd3a325ee477b2
joaovbressan <joaovictorteixeirabressan@gmail.com>
2025-04-30 20:24:42 -0300

    Adicionado Pacotes e Model

---
+++ OmniApI/Models/Alien.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmniApI.Models
+{
+    public class Alien
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public string Palneta { get; set; }
+    }
+}
