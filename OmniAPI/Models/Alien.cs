7a729bd8c875e95fc0a5ec8d9762d5c31f6d1365
joaovbressan <joaovictorteixeirabressan@gmail.com>
2025-04-30 19:56:13 -0300

    #

---
+++ OmniAPI/Models/Alien.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmniAPI.Models
+{
+    public class Alien
+    {
+        public int Id {get; set;}
+
+        public string Nome {get; set;}
+
+        public string Especie {get; set;}
+
+        public string Planeta {get; set;}
+
+    }
+}
