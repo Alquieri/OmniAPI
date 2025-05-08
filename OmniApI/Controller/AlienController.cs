61ef0fedcf9f726a19291e3575f10f576cb1164e
joaovbressan <joaovictorteixeirabressan@gmail.com>
2025-05-08 13:34:54 -0300

    Realizada a criação do Método PUT, Método DELET e colocado na models a configuração para criar id automáticamente.

--- OmniApI/Controller/AlienController.cs
+++ OmniApI/Controller/AlienController.cs
@@ -32,19 +32,62 @@ namespace OmniApI.Models.Controllers
         _appDbContext.Aliens.Add(alien);
         await _appDbContext.SaveChangesAsync();
 
-        return StatusCode(201, alien);
+        return Ok( alien);
     }
 
     [HttpGet] 
-     public async Task<ActionResult<IEnumerable<Alien>>> GetPersonagens()   
+     public async Task<ActionResult<IEnumerable<Alien>>> GetAliens()   
     {
     
-        var personagens = await _appDbContext.Aliens.ToListAsync();
-        return Ok(personagens);
+        var aliens = await _appDbContext.Aliens.ToListAsync();
+        return Ok(aliens);
 
     }
 
+    [HttpPut("{id}")] 
+public async Task<IActionResult> UpdateAlien(int id, [FromBody]Alien alienAtualizado)
+{
     
+    var alienExistente = await _appDbContext.Aliens.FindAsync(id);
+
+    
+    if (alienExistente == null)
+    {
+        return NotFound("Alien não encontrado no omnitrix.");
+    }
+
+    _appDbContext.Entry(alienExistente).CurrentValues.SetValues(alienAtualizado);
+
+    await _appDbContext.SaveChangesAsync();
+
+    return Ok(alienExistente);
+}
+ 
+
+ [HttpDelete("{id}")]
+public async Task<IActionResult> DeletePersonagem(int id)
+{
+   
+    var alien = await _appDbContext.Aliens.FindAsync(id);
+
+    
+    if (alien == null)
+    {
+        return NotFound("Alien não encontrado no omnitrix .");
+    }
+
+    
+    _appDbContext.Aliens.Remove(alien);
+
+   
+    await _appDbContext.SaveChangesAsync();
+
+    
+    return Ok("Alien deletado com sucesso do omnitrix!");
+}
+
+
+   
 
         
 
--- OmniApI/Models/Alien.cs
+++ OmniApI/Models/Alien.cs
@@ -1,9 +1,12 @@
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OmniApi.Models;
 
     public class Alien
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Nome { get; set; } 
         public string? Especie { get; set; }
